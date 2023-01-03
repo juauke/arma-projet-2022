bafdfa8410f1d7efebdfdf0cb4901981826dfeb8
Justin BABONNEAU <justin.babonneau@ensiie.fr>
2023-01-03 21:47:40 +0100

    Moved ldi instruction to avoid repeating it in the loop.

--- asm/q3_7/char_read.s
+++ asm/q3_7/char_read.s
@@ -1,5 +1,5 @@
-loop:	in r7 # r7 <- getchar()
 	ldi r5, 13 # r5 <- 13 = <Enter>
+loop:	in r7 # r7 <- getchar()
 	jeq r7, r5, end # if (r7 == r5) goto end
 	out r7 # putchar() <- r7
 	jmp loop # goto loop
