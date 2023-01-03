e49367204bc370047b98b3e21bcff68192c92c07
Justin BABONNEAU <justin.babonneau@ensiie.fr>
2023-01-03 21:51:52 +0100

    Removed typo from read2_mul.s

--- asm/q5_4/read2_mul.s
+++ asm/q5_4/read2_mul.s
@@ -5,7 +5,7 @@ init: 	in r0 # r0 <- getchar()
 int: 	subi r7, r0, 24 # r7 <- r0-24
 	 	subi r0, r7, 24 # r0 <- r7-24; convert r0 to its integer value
 
-	 	subi r7, r1, 24 # r7 <- r2-24;
+	 	subi r7, r1, 24 # r7 <- r2-24
 	 	subi r1, r7, 24 # r2 <- r7-24; convert r1 to its integer value
 
 calc:	mul r7, r0, r1 # r7 <- r0*r1 mod 256
