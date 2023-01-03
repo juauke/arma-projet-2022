d286d45ab9120d95c458e7dabfb6e9cf7f6dcaa2
Justin BABONNEAU <justin.babonneau@ensiie.fr>
2023-01-03 21:48:35 +0100

    Removed out since it is not always displayable.

--- asm/q5_3/div10.s
+++ asm/q5_3/div10.s
@@ -4,11 +4,10 @@
 
 		lsr r1, r0, 1 # r1 <- r0 >> 1 = r0/2
 
-loop:	jlt r7, r1, output # if (r7 < r5) goto output
+loop:	jlt r7, r1, end # if (r7 < r5) goto end
 		subi r1, r0, 5 # r1 <- r0-5
 		addi r3, r2, 1 # r3 <- r2+1
 		addi r2, r3, 0 # r2 <- r3
 		jmp loop # goto loop
 
-output:	out r3
 end:	jmp end # goto end
